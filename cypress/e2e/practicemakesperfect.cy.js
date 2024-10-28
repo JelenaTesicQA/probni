70e196d85a5ace4db6251ce2efa7b427ed41ade8
JelenaTDev <jelena.tesic.101@gmail.com>
2024-10-28 09:15:36 +0100

    komitovanje

--- cypress/e2e/practicemakesperfect.cy.js
+++ cypress/e2e/practicemakesperfect.cy.js
@@ -1,7 +1,8 @@
 describe('primeri testa', () => {
 
     it('primer1', () => {
-      cy.visit('www.blic.rs');
+      cy.visit('https://katalon-demo-cura.herokuapp.com/');
+      cy.get('a#btn-make-appointment', { timeout: 60000 }).should('contain','Make Appointment').click();
       });
 
 });
